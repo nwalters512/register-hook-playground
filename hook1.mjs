b668116b10151e0d7b083504be3b09c3cbb3a1a9
Nathan Sarang-Walters <nwalters512@gmail.com>
2024-05-14 11:43:10 -0700

    Simplify

--- hook1.mjs
+++
@@ -1,7 +0,0 @@
-import path from 'node:path';
-
-export async function resolve(specifier, context, parentResolve) {
-  const url = await parentResolve(specifier, context, parentResolve);
-  console.trace(`hook1 resolved ${specifier} (from ${context.parentURL}) to ${url.url}`)
-  return url;
-}
--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
 import { register } from 'node:module';
-// register("./hook1.mjs", import.meta.url);
-register("./hook2.mjs", import.meta.url);
+register("./hook.mjs", import.meta.url);
 
 await import('node:util');
